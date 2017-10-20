0b2d332e771830d900fe8e600599494909bfc0ba
shionn <shionn@gmail.com>
2017-10-20 11:53:12 +0000

    ST7920 fonctionne mais en non SPI

--- SpiSt7920.cxx
+++ SpiSt7920.cxx
@@ -27,33 +27,78 @@
 #include <wiringPiSPI.h>
 
 #define CHANNEL 0
+#define CS 0
+#define MOSI 12
+#define CLOCK 14
+
+
 
 void sendCommand(unsigned char command) {
 	unsigned char buffer[3];
 	buffer[0] = 0b11111000;
 	buffer[1] = command & 0b11110000;
 	buffer[2] = command << 4;
+    digitalWrite(CS, HIGH);
 	wiringPiSPIDataRW(CHANNEL, buffer, 3);
+    digitalWrite(CS, LOW);
+	
 }
 
-
-int main(int argc, char **argv)
-{
-    std::cout << "init" << wiringPiSPISetup(CHANNEL, 50000) << "\n";
+void locate(unsigned char x, unsigned char y) {
+	unsigned char buffer[5];
+	buffer[0] =   0b11111000;
+	buffer[1] = ( 0b10000000 + y ) & 0b11110000;
+	buffer[2] = ( 0b10000000 + y ) << 4;
+	buffer[3] = ( 0b10000000 + x ) & 0b11110000;
+	buffer[4] = ( 0b10000000 + x ) << 4;
 	
-	std::cout << "clear\n";
+    digitalWrite(CS, HIGH);
+	wiringPiSPIDataRW(CHANNEL, buffer, 5);
+	digitalWrite(CS, LOW);
+
+}
+
+void draw() {
+	unsigned char pattern = 0b11100111;
+    unsigned char buffer[3];
+	buffer[0] = 0b11111010;
+	
+	digitalWrite(CS, HIGH);
+	wiringPiSPIDataRW(CHANNEL, buffer, 1);
+	buffer[1] = pattern & 0b11110000;
+	buffer[2] = pattern << 4;
+	for (int y = 0; y< 16;y++) {
+		for (int x= 0; x<32;x++) {
+			wiringPiSPIDataRW(CHANNEL, buffer, 2);
+			delay(1);
+		}
+	}
+	digitalWrite(CS, LOW);
+}
+
+int main(int argc, char **argv) {
+	wiringPiSetup () ;
+	pinMode (CS, OUTPUT) ;
+
+    std::cout << "init" << wiringPiSPISetup(CHANNEL, 100000) << "\n";
+    
+	
 	sendCommand(0b00000001);
     delay(2000);
-	std::cout << "on\n";
 	sendCommand(0b00001100);
-    delay(2000);
-	std::cout << "ext 8 \n";
+    delay(100);
 	sendCommand(0b00110100);
-    delay(2000);
-	std::cout << "ext 8 gr\n";
+    delay(100);
 	sendCommand(0b00110110);
-    delay(2000);
+    delay(100);
+    
+    locate(0,0);
+    delay(500);
+    
+	draw();
 	
+		digitalWrite(CS, HIGH);
+
 	return 0;
 }
 
---
+++ St7920.cxx
@@ -0,0 +1,92 @@
+/*
+ * SpiSt7920.cxx
+ * 
+ * Copyright 2017  <pi@raspberrypi>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <wiringPi.h>
+
+#define CS 0
+#define MOSI 12
+#define CLOCK 14
+
+#define START digitalWrite(CS, HIGH)
+#define END digitalWrite(CS, LOW)
+
+void peak() {
+    digitalWrite(CLOCK, HIGH);
+    digitalWrite(CLOCK, LOW);
+}
+
+void write(unsigned char data) {
+	for (int i=0;i<8;i++) {
+		digitalWrite(MOSI, data & 0b10000000);
+		peak();
+		data = data << 1;
+	}
+}
+
+void writeword(unsigned char data) {
+	write(data & 0b11110000);
+	write(data << 4);
+}
+
+
+int main(int argc, char **argv) {
+	wiringPiSetup () ;
+	pinMode (CS, OUTPUT) ;
+	pinMode (MOSI, OUTPUT) ;
+	pinMode (CLOCK, OUTPUT) ;
+
+	START;
+	write(0b11111000);
+	writeword(0b00000001);
+    delay(2000);
+	writeword(0b00001100);
+    delay(1);
+	writeword(0b00110100);
+    delay(1);
+	writeword(0b00110110);
+    delay(1);
+	writeword(0b10000000);
+	writeword(0b10000000);
+    delay(1);
+	END;
+
+    delay(100);
+
+
+
+	START;
+	write(0b11111010);
+	writeword(0b11111111);
+	writeword(0b11111111);
+	writeword(0b11111111);
+	writeword(0b11111111);
+	writeword(0b11111111);
+	writeword(0b11111111);
+	END;
+	
+
+	return 0;
+}
+
