2412d3d26d38161498d3d1974d9ca176b5b8d83a
shionn <shionn@gmail.com>
2017-10-25 11:55:09 +0000

    Passe l'initialisation avec la lib BCM2835

--- SpiSt7920.cxx
+++ SpiSt7920.cxx
@@ -24,81 +24,90 @@
 
 #include <iostream>
 #include <wiringPi.h>
-#include <wiringPiSPI.h>
+//#include <wiringPiSPI.h>
+#include <bcm2835.h>
+
 
 #define CHANNEL 0
 #define CS 0
 #define MOSI 12
 #define CLOCK 14
 
+#define START digitalWrite(CS, HIGH)
+#define END digitalWrite(CS, LOW)
 
-
-void sendCommand(unsigned char command) {
-	unsigned char buffer[3];
+void sendCommand(char command) {
+	/*char buffer[3];
 	buffer[0] = 0b11111000;
 	buffer[1] = command & 0b11110000;
-	buffer[2] = command << 4;
-    digitalWrite(CS, HIGH);
-	wiringPiSPIDataRW(CHANNEL, buffer, 3);
-    digitalWrite(CS, LOW);
-	
+	buffer[2] = command << 4;*/
+    START;
+    bcm2835_spi_transfer(0b11111000);
+    bcm2835_spi_transfer(command & 0b11110000);
+    bcm2835_spi_transfer(command << 4);
+    
+	//bcm2835_spi_writenb(buffer, 3);
+    END;
 }
 
-void locate(unsigned char x, unsigned char y) {
-	unsigned char buffer[5];
-	buffer[0] =   0b11111000;
-	buffer[1] = ( 0b10000000 + y ) & 0b11110000;
-	buffer[2] = ( 0b10000000 + y ) << 4;
-	buffer[3] = ( 0b10000000 + x ) & 0b11110000;
-	buffer[4] = ( 0b10000000 + x ) << 4;
-	
-    digitalWrite(CS, HIGH);
-	wiringPiSPIDataRW(CHANNEL, buffer, 5);
-	digitalWrite(CS, LOW);
-
+void locate() {
+	START;
+    bcm2835_spi_transfer(0b11111000);
+	bcm2835_spi_transfer(0b10000000);
+	bcm2835_spi_transfer(0);
+	bcm2835_spi_transfer(0b10000000);
+	bcm2835_spi_transfer(0);
+	END;
 }
 
-void draw() {
-	unsigned char pattern = 0b11100111;
-    unsigned char buffer[3];
+void sendData(char data) {
+	/*char buffer[3];
 	buffer[0] = 0b11111010;
-	
-	digitalWrite(CS, HIGH);
-	wiringPiSPIDataRW(CHANNEL, buffer, 1);
-	buffer[1] = pattern & 0b11110000;
-	buffer[2] = pattern << 4;
-	for (int y = 0; y< 16;y++) {
-		for (int x= 0; x<32;x++) {
-			wiringPiSPIDataRW(CHANNEL, buffer, 2);
-			delay(1);
-		}
-	}
-	digitalWrite(CS, LOW);
+	buffer[1] = data & 0b11110000;
+	buffer[2] = data << 4;*/
+    START;
+    bcm2835_spi_transfer(0b11111010);
+    bcm2835_spi_transfer(data & 0b11110000);
+    bcm2835_spi_transfer(data << 4);
+    
+	//bcm2835_spi_writenb(buffer, 3);
+    END;
 }
+
 
 int main(int argc, char **argv) {
-	wiringPiSetup () ;
+	std::cout << wiringPiSetup () << "\n";
 	pinMode (CS, OUTPUT) ;
-
-    std::cout << "init" << wiringPiSPISetup(CHANNEL, 100000) << "\n";
-    
+	std::cout << bcm2835_init() << "\n";
+	
+	std::cout <<  bcm2835_spi_begin() << "\n";
+	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE3);                   // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, HIGH);      // the default
+	
 	
 	sendCommand(0b00000001);
     delay(2000);
 	sendCommand(0b00001100);
-    delay(100);
+    delay(1);
 	sendCommand(0b00110100);
-    delay(100);
+    delay(1);
 	sendCommand(0b00110110);
-    delay(100);
+    delay(1);
     
-    locate(0,0);
-    delay(500);
+    locate();
+    delay(1);
+    sendData(0xFF);
+    sendData(0b00111100);
+    sendData(0b11001100);
     
-	draw();
+    
+    
+    bcm2835_spi_end();
+    bcm2835_close();
 	
-		digitalWrite(CS, HIGH);
-
 	return 0;
 }
 
